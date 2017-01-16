ad4db2453e05815a21addc263427594017519fad
Harry Reeder <harry@harryreeder.co.uk>
2017-01-16 13:25:07 +0000

    Start project logging

--- src/main/kotlin/manager/manager.kt
+++ src/main/kotlin/manager/manager.kt
@@ -1,13 +1,17 @@
 package manager
 
+import org.slf4j.LoggerFactory
 import spark.Spark
 import spark.Spark.get
 
 fun main(args: Array<String>) {
     val PORT: Int = 8095
+    val logger = LoggerFactory.getLogger("Manager")
+
+    logger.info("Starting BotBot Manager, Please Wait")
+
     Spark.port(PORT)
 
+    // Routing
     get("/", { req, res -> "Hello" })
-
-    println("Listening On 0.0.0.0:$PORT")
 }
